4337fceaafc83309b2d989898869089676f1b217
Romario <grajalesromario@gmail.com>
2023-08-31 11:26:42 -0500

    se suben todos los archivos al repositorio

---
+++ logica.js
@@ -0,0 +1,14 @@
+var salarioInicial;
+var salarioAjustado;
+var ajuste;
+ajuste=parseFloat(ajuste);
+salarioInicial=parseFloat(prompt("Ingrese su salario"));
+
+if (salarioInicial<1300606) {
+    ajuste=salarioInicial*0.15
+} else {
+    ajuste=0
+}
+salarioAjustado=ajuste+salarioInicial;
+alert("su salario ajustado es" + salarioAjustado);
+console.log("su salario con ajuste es igual a" + salarioAjustado);
